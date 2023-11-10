d54147b70d6db8e75c40545b4aec3a1df016b827
luqmanamirullah <amirullahluqman@gmail.com>
2023-11-10 15:28:52 +0700

    feat: add role-based authorization.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,32 +1,70 @@
+const { prisma } = require('../configs/prisma.config');
 const { verifyToken, errorResponse } = require('../utils/helper.util');
 
-const checkToken = (req, res, next) => {
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+
+const auth = (role) => async (req, res, next) => {
   const { refreshToken } = req.cookies;
   if (!refreshToken) {
-    errorResponse(res, 'Forbidden refresh token is not found', null, 403);
+    return errorResponse(
+      res,
+      'Forbidden, refresh token is not found',
+      null,
+      403,
+    );
   }
 
-  const user = verifyToken(refreshToken);
-  if (user instanceof Error) {
-    errorResponse(res, 'Forbidden refresh token is invalid', null, 403);
-  }
-
-  next();
-};
-
-const authToken = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    errorResponse(res, 'Forbidden authorization token is not found', null, 403);
+    return errorResponse(
+      res,
+      'Forbidden authorization token is not found',
+      null,
+      403,
+    );
   }
 
   const accessToken = authorization.split(' ')[1];
-  const accessTokenIsValid = verifyToken(accessToken);
-  if (accessTokenIsValid instanceof Error) {
-    errorResponse(res, 'Unauthorized', null, 401);
+  const decoded = verifyToken(accessToken);
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decoded.id,
+    },
+  });
+
+  if (!user) {
+    return errorResponse(
+      res,
+      'Forbidden, you are not allowed access this resource',
+      null,
+      403,
+    );
+  }
+  if (decoded instanceof Error) {
+    return errorResponse(
+      res,
+      'Unauthenticated, you are not logged in',
+      null,
+      401,
+    );
   }
 
-  next();
+  if (role !== undefined) {
+    if (decoded.role.name !== role && decoded.role.name !== 'Super Admin') {
+      return errorResponse(
+        res,
+        'Unauthorized, you are not allowed to access this resource',
+        null,
+        401,
+      );
+    }
+  }
+
+  return next();
 };
 
-module.exports = { checkToken, authToken };
+module.exports = auth;
