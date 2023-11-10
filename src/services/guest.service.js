df47ac44820f711c86997c391029ef40ec2b715c
luqmanamirullah <amirullahluqman@gmail.com>
2023-11-10 15:48:43 +0700

    refactor : get function in guest service to handle errors properly

--- src/services/guest.service.js
+++ src/services/guest.service.js
@@ -7,22 +7,22 @@ const { errorResponse, successResponse } = require('../utils/helper.util');
  */
 
 async function get(req, res) {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: parseInt(req.params.id, 10),
-    },
-  });
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        id: parseInt(req.params.id, 10),
+      },
+    });
 
-  if (!user) {
+    return successResponse(
+      res,
+      `User ${req.params.id} has been getted successfully`,
+      user,
+      200,
+    );
+  } catch (error) {
     return errorResponse(res, 'User not found', '', 404);
   }
-
-  return successResponse(
-    res,
-    `User ${req.params.id} has been getted successfully`,
-    user,
-    200,
-  );
 }
 
 module.exports = { get };
