a775094bda8824c968b46f9732ecb5b72127d6d1
luqmanamirullah <amirullahluqman@gmail.com>
2023-11-10 15:47:52 +0700

    feat: authentication middleware and update homepage response

--- index.js
+++ index.js
@@ -6,24 +6,20 @@ const port = config.port || 3000;
 const guestRouter = require('./src/routes/guest.route');
 const authRouter = require('./src/routes/auth.route');
 const { configServer } = require('./src/configs/server.config');
+const middleware = require('./src/middlewares/auth.middleware');
 
 const app = express();
 
 configServer(app);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'dedek wawan berjalan dengan benar' });
+  res.status(200).render('<p >Hello World</p>');
 });
 
-app.use('/guest', guestRouter);
 app.use('/auth', authRouter);
 
-/* Error handler middleware */
-app.use((req, res, err) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ message: err.message });
-});
+app.use(middleware(['Admin']));
+app.use('/guest', guestRouter);
 
 app.listen(port, (err) => {
   if (err) console.error(err);
