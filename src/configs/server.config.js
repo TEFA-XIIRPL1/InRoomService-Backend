9e245acaf36b5f9b05476dd94c8948c7f1a5c7c0
luqmanamirullah <amirullahluqman@gmail.com>
2023-11-15 13:04:02 +0700

    feat: add CORS support to server.config.js

--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -15,6 +15,19 @@ const configServer = (app) => {
       extended: true,
     }),
   );
+  // cors
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Controll-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    return next();
+  });
 };
 
 module.exports = { configServer };
